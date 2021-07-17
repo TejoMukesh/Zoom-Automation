20c0d696e72e96314808a162080eeba41a40d902
TejoMukesh <38899975+TejoMukesh@users.noreply.github.com>
2021-07-17 10:11:16 +0530

    Update and rename mainwithsms.py to zoom_vanced.py

--- zoom_vanced.py
+++ zoom_vanced.py
@@ -1,14 +1,10 @@
-from datetime import *
+from datetime import date
 import webbrowser
-import calendar
-import sched
+import calendar, sched
 import time
 from twilio.rest import Client
 
-''' 
-This is essentially the same script as main.py but if you have a free Twilio account and would like to get a text message
-when it's time for class, you can use this script to do that. You'll have to update your Twilio account sid and auth id accordingly.
-'''
+
 
 bio =   'https://us04web.zoom.us/j/123456789/pwd=?'
 phys = ''
@@ -16,18 +12,20 @@ maths = ''
 eng =''
 chem = ''
 
-today_date = str(date.today())
-day_of_week = calendar.day_name[date.today().weekday()]
-
 '''Change the contents of this dictionary (time table) accordingly.''' 
 
 class_times ={
-    'Monday':[['09 30',bio],['10 30',maths],['11 30',eng],['12 30',chem],['14 10',phys]],
-               'Tuesday':[['09 30',bio],['10 30',chem],['11 30',maths],['12 30',chem],['14 10',eng]],
-               'Wednesday':[['09 30',phys],['10 30',maths],['11 30',bio],['12 30',chem],['14 10',eng]],
-               'Thursday':[['09 30',phys],['10 30',eng],['11 30',maths],['12 30',chem],['14 10',bio]],
-               'Friday':[['09 30',chem],['10 30',bio],['11 30',maths],['12 30',phys],['14 10',eng]],
-    'Saturday':[['09 30',chem],['10 30',bio],['11 30',maths],['12 30',eng],['14 10',phys]]
+    'Monday':[['09 00',phys],['10 15',maths],['11 30',bio],['13 15',chem],['14 30',phys]],
+
+               'Tuesday':[['09 00',phys],['10 15',eng],['11 30',bio],['13 15',chem],['14 30',maths]],
+
+               'Wednesday':[['09 00',bio],['10 15',phys],['11 30',maths],['13 15',chem],['14 30',bio]],
+
+               'Thursday': [['09 00',maths],['10 15',chem],['11 30',maths],['13 15',bio],['14 30',phys]],
+
+               'Friday':[['09 00',bio],['10 15',chem],['11 30',phys],['13 15',maths],['14 30',chem]],
+
+    'Saturday':[['09 00',phys],['10 15',eng],['11 30',bio],['13 15',chem],['14 30',maths]]
     }
 
 
@@ -44,6 +42,9 @@ my_msg = "Time for class"
 'uncomment this line'
 #client = Client(account_sid,auth_token)			
 
+today_date = str(date.today())
+day_of_week = calendar.day_name[date.today().weekday()]
+
 x= sched.scheduler(time.time,time.sleep)
 
 def zoom_class(link):
@@ -51,8 +52,8 @@ def zoom_class(link):
         'uncomment this line'
         #message = client.messages.create(to = my_cell, from_ =my_twilio, body = my_msg )
         
-def get_new_time(old_time):
-    added_minutes=int(old_time[3:]) + 40
+def get_new_time(old_time, add):
+    added_minutes=int(old_time[3:]) + add
     if added_minutes >= 60:
         new_time = str(str(int(old_time[:2]) + 1) + ' ' + str(added_minutes - 60))
     else:
@@ -65,7 +66,7 @@ def attend():
     if day_of_week in class_times.keys():
         class_no = 0 #this is for the loop
         
-        for i in range(5):
+        for i in class_times[day_of_week]:
             if time.strptime(f'{today_date} {class_times[day_of_week][class_no][0]}','%Y-%m-%d %H %M') > time.localtime():
                 x.enterabs((time.mktime(time.strptime(f'{today_date} {class_times[day_of_week][class_no][0]}',"%Y-%m-%d %H %M"))),0,zoom_class,kwargs={'link':class_times[day_of_week][class_no][1]})
                 #to rejoin after 40 mins
